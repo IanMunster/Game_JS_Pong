5afb3828c98fff6d76879a5abe207bd5e31df854
I Munster <I.Munster92@gmail.com>
2023-05-30 15:53:49 +0200

    fixed depricated KeyCode

--- js_pong.js
+++ js_pong.js
@@ -1,5 +1,6 @@
 // Game State
-var gameState = false;
+var gameState = false,
+	frameUpdate = 10;
 
 // Page Loaded
 window.addEventListener("load", function () {
@@ -17,8 +18,8 @@ window.addEventListener("load", function () {
 		scoreSFX = new Audio(),
 		soundState = false;
 
-		bounceSFX.src = "sound/pong.wav";
-		scoreSFX.src = "sound/score.wav";
+	bounceSFX.src = "sound/pong.wav";
+	scoreSFX.src = "sound/score.wav";
 
 	// Ball Movement
 	var posX = 100,
@@ -42,12 +43,13 @@ window.addEventListener("load", function () {
 
 		if (gameState) {
 			btn_start.value = 'Pause Game';
+			playGame();
 
-		//	draw();
+			//btn_start.style.display = 'none';
 		} else {
 			btn_start.value = 'Start Game';
 		}
-		console.log("Play Game: "+gameState);
+		console.log("Play Game: " + gameState);
 	})
 
 	// Sound Button
@@ -73,98 +75,105 @@ window.addEventListener("load", function () {
 	// User Input
 	window.addEventListener("keydown", function (e) {
 		//Player 1 Movement
-		if (e.keyCode == 87 && leftY != 0) {
+		if (e.key == 'w' && leftY != 0) {
 			leftY -= 10;
 		}
-		if (e.keyCode == 83 && leftY != 350) {
+		if (e.key == 's' && leftY != 350) {
 			leftY += 10;
 		}
 
 		// Player 2 Movement
-		if (e.keyCode == 38 && rightY != 0) {
+		if (e.key == 'ArrowUp' && rightY != 0) {
 			rightY -= 10;
 		}
-		if (e.keyCode == 40 && rightY != 350) {
+		if (e.key == 'ArrowDown' && rightY != 350) {
 			rightY += 10;
 		}
 	})
 
-	// if (gameState) {
-	console.log("Start Draw");
+	function playGame() {
+		var drawInterval;
 
-	function draw() {
-		// Draw Pong Ball
-		context.clearRect(0, 0, 800, 450);
-		context.fillStyle = "red";
-		context.fillRect(posX, posY, 50, 50);
-		// Draw Player 1
-		context.fillStyle = "yellow";
-		context.fillRect(10, leftY, 15, 100);
-		// Draw Player 2
-		context.fillStyle = "green";
-		context.fillRect(770, rightY, 15, 100);
+		if (gameState) {
 
-		// Pong Ball Movement
-		posX += stapX;
-		posY += stapY;
+			function draw() {
+				console.log("Start Draw");
+				// Draw Pong Ball
+				context.clearRect(0, 0, 800, 450);
+				context.fillStyle = "red";
+				context.fillRect(posX, posY, 50, 50);
+				// Draw Player 1
+				context.fillStyle = "yellow";
+				context.fillRect(10, leftY, 15, 100);
+				// Draw Player 2
+				context.fillStyle = "green";
+				context.fillRect(770, rightY, 15, 100);
 
-		// Pong Ball Collision Player 1
-		if (posX >= 0 &&
-			posX <= 20 &&
-			posY >= leftY - 80 &&
-			posY <= leftY + 100) {
-			stapX = -stapX;
-			if (soundState) {
-				bounceSFX.playbackRate = 0.25;
-				bounceSFX.play()
-			};
+				// Pong Ball Movement
+				posX += stapX;
+				posY += stapY;
+
+				// Pong Ball Collision Player 1
+				if (posX >= 0 &&
+					posX <= 20 &&
+					posY >= leftY - 80 &&
+					posY <= leftY + 100) {
+					stapX = -stapX;
+					if (soundState) {
+						bounceSFX.playbackRate = 0.25;
+						bounceSFX.play()
+					};
+				}
+
+				// Pong Ball Collision Player 2
+				if (posX + 50 >= 775 &&
+					posX + 50 <= 800 &&
+					posY >= rightY - 100 &&
+					posY <= rightY + 80) {
+					stapX = -stapX;
+					if (soundState) {
+						bounceSFX.playbackRate = 0.25;
+						bounceSFX.play()
+					};
+				}
+
+				// Pong Ball Score Player 1
+				if (posX > 750) {
+					posX = 400;
+					scoreP1++;
+					displayScore1.innerHTML = scoreP1;
+					if (soundState) {
+						scoreSFX.play()
+					};
+				}
+				// Pong Ball Score Player 2
+				if (posX < 0) {
+					posX = 400;
+					scoreP2++;
+					displayScore2.innerHTML = scoreP2;
+					if (soundState) {
+						scoreSFX.play()
+					};
+				}
+
+				// Pong Ball Bounce Wall
+				if (posY < 0 || posY > 400) {
+					stapY = -stapY;
+					if (soundState) {
+						bounceSFX.playbackRate = bounceSFX.defaultPlaybackRate;
+						bounceSFX.play()
+					};
+				}
+			}
+
+			// Canvas Draw Update
+			drawInterval = setInterval(draw, frameUpdate);
+
+		} else {
+			clearInterval(drawInterval);
 		}
 
-		// Pong Ball Collision Player 2
-		if (posX + 50 >= 775 &&
-			posX + 50 <= 800 &&
-			posY >= rightY - 100 &&
-			posY <= rightY + 80) {
-			stapX = -stapX;
-			if (soundState) {
-				bounceSFX.playbackRate = 0.25;
-				bounceSFX.play()
-			};
-		}
-
-		// Pong Ball Score Player 1
-		if (posX > 750) {
-			posX = 400;
-			scoreP1++;
-			displayScore1.innerHTML = scoreP1;
-			if (soundState) {
-				scoreSFX.play()
-			};
-		}
-		// Pong Ball Score Player 2
-		if (posX < 0) {
-			posX = 400;
-			scoreP2++;
-			displayScore2.innerHTML = scoreP2;
-			if (soundState) {
-				scoreSFX.play()
-			};
-		}
-
-		// Pong Ball Bounce Wall
-		if (posY < 0 || posY > 400) {
-			stapY = -stapY;
-			if (soundState) {
-				bounceSFX.playbackRate = bounceSFX.defaultPlaybackRate;
-				bounceSFX.play()
-			};
-		}
 	}
-
-	// Canvas Draw Update
-	setInterval(draw, 10);
-
-	// }
 
 })
 
